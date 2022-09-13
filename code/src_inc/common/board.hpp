8d48903f62394620d0fce02e6810f4cbd9ad8664
a-red-ant <cgr1963@gmail.com>
2022-09-13 18:48:31 +0100

    added files and some instructions.

---
+++ code/src_inc/common/board.hpp
@@ -0,0 +1,6 @@
+
+// You have to define a class called board to represent the board of the game
+// Some of the function could be:
+// add_chip( row, column, player)
+// is_empty( row_ column )
+// winner() should return NONE, PLAYER_1, PLAYER_2 (those values can be an enum.
